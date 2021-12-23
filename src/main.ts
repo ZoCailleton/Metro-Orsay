21dd2c2546a6a83f4169712a87ed6b0a0bcbe17e
Zo <enzo.cailleton2@gmail.com>
2021-12-23 15:01:14 +0100

    Démo Parallax GSAP

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,23 @@
-import './style.css'
+import './style.scss'
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+import gsap, { Power0 } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
+
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#slide-1",
+    pin: true,
+    start: 'top top',
+    end: 'bottom top',
+    markers: true,
+    scrub: true
+  }
+});
+
+tl
+.to('#slide-1 .layer:nth-child(1)', {scale: 1.25, duration: 1, ease: Power0.easeNone})
+.to('#slide-1 .layer:nth-child(2)', {scale: 1.5, duration: 1, ease: Power0.easeNone}, '-=1')
+.to('#slide-1 .layer:nth-child(3)', {scale: 1.75, duration: 1, ease: Power0.easeNone}, '-=1')
+.to('#slide-1 .layer:nth-child(4)', {scale: 2, duration: 1, ease: Power0.easeNone}, '-=1')
