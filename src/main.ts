f2c611a6fd5f6412a4dafcc136f0f7f2524eb8b0
Maelle05 <maelle.rabouan@gmail.com>
2022-01-03 18:32:58 +0100

    feat() class paralax

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,15 @@ import { SlideSubtitle } from './voix'
 
 import gsap, { Power2 } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import { getShift } from './mouse'
+import { MouseParalax } from './mouse'
 
 gsap.registerPlugin(ScrollTrigger)
 
 const cursor = document.querySelector<HTMLInputElement>('.cursor')
 window.addEventListener('mousemove', (e: MouseEvent) => {
-    let _x = e.clientX - cursor!.offsetWidth / 2
-    let _y = e.clientY - cursor!.offsetHeight / 2
-    cursor?.setAttribute('style', 'transform: translate(' + _x + 'px, ' + _y + 'px)')
+  let _x = e.clientX - cursor!.offsetWidth / 2
+  let _y = e.clientY - cursor!.offsetHeight / 2
+  cursor?.setAttribute('style', 'transform: translate(' + _x + 'px, ' + _y + 'px)')
 })
 
 let CURRENT_SCENE: number = 0
@@ -27,7 +27,7 @@ intro
   .to('.intro .side:last-child', { rotation: 10, opacity: 0, duration: 1, ease: Power2.easeInOut }, '-=1')
   .to('.slide-1', { width: '100vw', height: '100vh', marginTop: 0, borderRadius: 0, duration: 1, ease: Power2.easeInOut }, '-=1')
   .to('.nav--bottom', { bottom: 0, duration: 1, ease: Power2.easeInOut }, '-=1')
-  .to('.intro', {opacity: 0})
+  .to('.intro', { opacity: 0 })
 
 document.querySelector<HTMLInputElement>('.intro .btn')?.addEventListener('click', () => {
   intro.play()
@@ -51,25 +51,25 @@ slide1?.setAttribute('style', 'margin-top: -100px')
 const scene_1_to_2 = gsap.timeline({ paused: true })
 
 scene_1_to_2
-.to('.slide-1 .layer img', {x: 0, y: 0, duration: 0.5, ease: Power2.easeInOut})
-.to('.slide-1 .layer:nth-child(8)', {opacity: 0, duration: 0.5, ease: Power2.easeInOut})
-.to('.slide-1 .layer:nth-child(7)', {opacity: 0, duration: 0.5, ease: Power2.easeInOut})
-.to('.slide-1 .layer:nth-child(6)', {opacity: 0, duration: 0.5, ease: Power2.easeInOut})
-.to('.slide-1 .layer:nth-child(2)', {opacity: 0, duration: 0.5, ease: Power2.easeInOut})
-.to('.slide-1 .layer:nth-child(3)', {opacity: 0, duration: 0.5, ease: Power2.easeInOut}, '-=0.5')
-.to('.slide-1 .layer:nth-child(4)', {opacity: 0, duration: 0.5, ease: Power2.easeInOut}, '-=0.5')
-.to('.slide-1 .layer:nth-child(5)', {opacity: 0, duration: 0.5, ease: Power2.easeInOut}, '-=0.5')
+  .to('.slide-1 .layer img', { x: 0, y: 0, duration: 0.5, ease: Power2.easeInOut })
+  .to('.slide-1 .layer:nth-child(8)', { opacity: 0, duration: 0.5, ease: Power2.easeInOut })
+  .to('.slide-1 .layer:nth-child(7)', { opacity: 0, duration: 0.5, ease: Power2.easeInOut })
+  .to('.slide-1 .layer:nth-child(6)', { opacity: 0, duration: 0.5, ease: Power2.easeInOut })
+  .to('.slide-1 .layer:nth-child(2)', { opacity: 0, duration: 0.5, ease: Power2.easeInOut })
+  .to('.slide-1 .layer:nth-child(3)', { opacity: 0, duration: 0.5, ease: Power2.easeInOut }, '-=0.5')
+  .to('.slide-1 .layer:nth-child(4)', { opacity: 0, duration: 0.5, ease: Power2.easeInOut }, '-=0.5')
+  .to('.slide-1 .layer:nth-child(5)', { opacity: 0, duration: 0.5, ease: Power2.easeInOut }, '-=0.5')
 
 document.querySelector<HTMLInputElement>('.slide-1')?.addEventListener('click', () => {
-  if(CURRENT_SCENE === 1) {
+  if (CURRENT_SCENE === 1) {
     // Si l'animation d'intro est terminée
-    if(intro.totalProgress() === 1) {
+    if (intro.totalProgress() === 1) {
       scene_1_to_2.play()
       CURRENT_SCENE = 2
     }
-  } else if(CURRENT_SCENE === 2) {
+  } else if (CURRENT_SCENE === 2) {
     // Si la scène 1 est terminée
-    if(scene_1_to_2.totalProgress() === 1) {
+    if (scene_1_to_2.totalProgress() === 1) {
       scene_2_to_3.play()
       CURRENT_SCENE = 3
     }
@@ -86,43 +86,29 @@ const slide2 = document.querySelector<HTMLInputElement>('.slide-2')
 const scene_2_to_3 = gsap.timeline({ paused: true })
 
 scene_2_to_3
-.to('.slide-1 .layer:nth-child(9)', {scale: 1.5, opacity: 0, duration: 1, ease: Power2.easeInOut})
-.to('.slide-1 .layer:nth-child(1)', {scale: 1.25, opacity: 0, duration: 1, ease: Power2.easeInOut}, '-=1')
-.to('.slide-1', {scale: 1.15, opacity: 0, duration: 1, ease: Power2.easeInOut}, '-=1')
-.to('.slide-2', {scale: 1.1, duration: 1, ease: Power2.easeInOut}, '-=1')
-
-
-
+  .to('.slide-1 .layer:nth-child(9)', { scale: 1.5, opacity: 0, duration: 1, ease: Power2.easeInOut })
+  .to('.slide-1 .layer:nth-child(1)', { scale: 1.25, opacity: 0, duration: 1, ease: Power2.easeInOut }, '-=1')
+  .to('.slide-1', { scale: 1.15, opacity: 0, duration: 1, ease: Power2.easeInOut }, '-=1')
+  .to('.slide-2', { scale: 1.1, duration: 1, ease: Power2.easeInOut }, '-=1')
 
 
 
 /* Parallax sur toutes les sections */
-  
-document.addEventListener('mousemove', (e: MouseEvent) => {
 
-  let _w = window.innerWidth
-  let _h = window.innerHeight
-  let _mouseX = e.clientX
-  let _mouseY = e.clientY
+const scene1Paralax = new MouseParalax(1, slide1)
 
-  if (CURRENT_SCENE === 1 && intro.totalProgress() === 1) {
+function parallax() {
 
-    for (let layer of slide1!.querySelectorAll('.layer img')) {
-      let _coefBase = (layer as HTMLImageElement).dataset.coef || ''
-      let _coef: number = +_coefBase
-      layer?.setAttribute('style', 'transform: translate(' + getShift(_mouseX, _w, _coef * 0.01) + '%,' + getShift(_mouseY, _h, _coef * 0.01) + '%)')
-    }
-
-  } else if(CURRENT_SCENE === 2 && scene_1_to_2.totalProgress() === 1) {
-
-    for (let layer of slide2!.querySelectorAll('.layer img')) {
-      let _coefBase = (layer as HTMLImageElement).dataset.coef || ''
-      let _coef: number = +_coefBase
-      layer?.setAttribute('style', 'transform: translate(' + getShift(_mouseX, _w, _coef * 0.01) + '%,' + getShift(_mouseY, _h, _coef * 0.01) + '%)')
-    }
-
+  if (CURRENT_SCENE === 1 && intro.totalProgress() === 1 && scene1Paralax.getInit() === false) {
+    scene1Paralax.init()
   }
 
-})
+  requestAnimationFrame(parallax)
+}
+parallax()
+
+
+
+
 
 
--- src/mouse.ts
+++ src/mouse.ts
@@ -1,10 +1,69 @@
-export function getPercentage(value: number, maxValue: number) {
-    return 100 * value / maxValue
+function getPercentage(value: number, maxValue: number) {
+	return 100 * value / maxValue
 }
 
-export function getShift(mouse: number, window: number, coef: number) {
-    const shift = (mouse - window / 2) * coef
-    const mouseShift = getPercentage(shift, window)
+function getShift(mouse: number, window: number, coef: number) {
+	const shift = (mouse - window / 2) * coef
+	const mouseShift = getPercentage(shift, window)
 
-    return mouseShift
+	return mouseShift
+}
+
+export class MouseParalax {
+	scene: number;
+	isInit: boolean;
+	isStart: boolean;
+	containerEl: HTMLInputElement;
+	_w: number;
+	_h: number;
+	_mouseX: number;
+	_mouseY: number;
+	constructor(scene: number, containerEl: HTMLInputElement) {
+		this.scene = scene
+		this.isStart = false
+		this.containerEl = containerEl
+		this._w = window.innerWidth
+		this._h = window.innerHeight
+		this._mouseX = 0
+		this._mouseY = 0
+		this.isInit = false
+	}
+
+	init() {
+		document.addEventListener('mousemove', (e: MouseEvent) => {
+			this._mouseX = e.clientX
+			this._mouseY = e.clientY
+		})
+
+		this.isInit = true
+		this.isStart = true
+		this.loop()
+	}
+
+	start() {
+		this.isStart = true
+	}
+
+	stop() {
+		this.isStart = false
+	}
+
+	getInit() {
+		if (this.isInit === false) {
+			return false
+		} else {
+			return true
+		}
+	}
+
+	private loop() {
+		if (this.isStart === true) {
+			for (let layer of this.containerEl!.querySelectorAll('.layer img')) {
+				let _coefBase = (layer as HTMLImageElement).dataset.coef || ''
+				let _coef: number = +_coefBase
+				layer?.setAttribute('style', 'transform: translate(' + getShift(this._mouseX, this._w, _coef * 0.01) + '%,' + getShift(this._mouseY, this._h, _coef * 0.01) + '%)')
+			}
+		}
+		requestAnimationFrame(this.loop.bind(this));
+	}
 }
