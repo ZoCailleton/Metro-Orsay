ce32c59f4d64818cb68baef60f02d43fab4fd7ae
Maelle05 <maelle.rabouan@gmail.com>
2022-01-16 16:23:10 +0100

    fix loader

--- src/loader.ts
+++ src/loader.ts
@@ -45,7 +45,7 @@ export class LoaderTrigger {
         const currentRate = this.getRate()
 
         const increment = setInterval(() => {
-            if (currentRate !== this.lastRate) {
+            if (currentRate !== this.lastRate && this.lastRate < 100) {
                 this.domTextParagraph.innerHTML = this.lastRate + ' %'
                 this.lastRate++
             } else if (currentRate !== 100) {
