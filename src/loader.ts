b96f317dcaa15660e9532288cbbe0fa8e1ec8ff8
Maelle05 <maelle.rabouan@gmail.com>
2022-01-26 18:47:54 +0100

    loader

--- src/loader.ts
+++ src/loader.ts
@@ -1,70 +1,46 @@
 export class LoaderTrigger {
 
     container: any
-    text: string
+    text: any
     imagesList: any
-    imagesOnLoad: Array<HTMLImageElement>
-    numberImages: number
     numberImagesLoad: number
-    domTextParagraph: any
-    lastRate: number
-    isFinish: boolean
+    numberImages: number
+    rate: number
 
     constructor() {
         this.container = document.querySelector('#loader')
-        this.domTextParagraph = document.querySelector('#loader > p')
-        this.text = '0%'
-        this.imagesList
+        this.text = document.querySelector('#loader > p')
         this.numberImages = 0
         this.numberImagesLoad = 0
-        this.imagesOnLoad = []
-        this.lastRate = 0
-        this.isFinish = false
+        this.rate = 0
     }
 
     init() {
         this.imagesList = document.images
         this.numberImages = this.imagesList.length
-        this.imagesOnLoad = this.imagesList
+        this.text.innerHTML = this.rate + ' %'
+
+        console.log('Images List : ', this.imagesList)
+        console.log('Nombre total d images ' + this.numberImages)
         this.checkLoad()
     }
 
     private checkLoad() {
         this.numberImagesLoad = 0
-        for (let i = 0; i < this.imagesOnLoad.length; i++) {
-            if (this.imagesOnLoad[i].complete) {
-                this.numberImagesLoad++
-            }
+        for (let i = 0; i < this.imagesList.length; i++) {
+            this.imagesList[i].complete ? this.numberImagesLoad++ : null
+
         }
+        console.log('Nombre total d images load ' + this.numberImagesLoad)
 
-        setTimeout(() => this.updateDom(), 10)
-    }
+        this.rate = this.getRate()
 
-    private updateDom() {
-        // console.log('Load state :' + this.getRate())
-        const currentRate = this.getRate()
+        this.text.innerHTML = this.rate + ' %'
 
-        const increment = setInterval(() => {
-            if (currentRate !== this.lastRate && this.lastRate !== 100) {
-                this.domTextParagraph.innerHTML = this.lastRate + ' %'
-                this.lastRate++
-            } else if (currentRate !== 100) {
-                this.lastRate = currentRate
-                this.domTextParagraph.innerHTML = currentRate + ' %'
-            } else if (currentRate >= 100 || this.lastRate >= 100) {
-                clearInterval(increment)
-                this.domTextParagraph.innerHTML = '100%'
-                this.lastRate++
-                setTimeout(() => {
-                    this.container.classList.add('remove')
-                    this.isFinish = true
-                }, 1000)
-            }
 
-        }, 50)
-
-        if (this.numberImagesLoad !== this.numberImages) this.checkLoad()
-        this.domTextParagraph.innerHTML = currentRate + ' %'
+        setTimeout(() => {
+            this.numberImagesLoad === this.numberImages ? setTimeout(() => { this.container.style.display = 'none' }, 200) : this.checkLoad()
+        }, 20)
     }
 
     private getRate() {
